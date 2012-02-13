81555f01622a4b5208fdc158fe7809dc750815ac
Amr Tj. Wallas <eminem_virus2010@hotmail.com>
2012-02-13 13:33:24 +0200

    Weird var keywords are weird. Here you are, JS xD

--- lite-youtube-min.js
+++ lite-youtube-min.js
@@ -1,1 +1,18 @@
-(function(b){function c(){return b("div.lite").each(function(){myDiv=this,vid=this.id,w=this.style.width,h=this.style.height,img=b(document.createElement("img")),img.attr({"class":"lazy","data-original":"http://img.youtube.com/vi/"+vid+"/0.jpg",width:w,height:h}),img.css({position:"relative",top:"0",left:"0"}),img.lazyload({effect:"fadeIn"}),a=b(document.createElement("a")),a.href="#",a.click(function(){return b(this).replaceWith('<embed src="http://www.youtube.com/v/'+vid+'?version=3&autoplay=1" type="application/x-shockwave-flash" width="'+w+'" height="'+h+'" allowscriptaccess="always"></embed>'),!1}),b(myDiv).append(a.append(img,'<img class="lite" src="http://lh4.googleusercontent.com/-QCeB6REIFlE/TuGUlY3N46I/AAAAAAAAAaI/9-urEUtpKcI/s800/youtube-play-button.png" style="position: absolute; top: 50%; left: 45%;" />')),b.getJSON("http://gdata.youtube.com/feeds/api/videos/"+vid+"?v=2&fields=id,title&alt=json",function(a){this.innerHTML+='<div style="position:relative;margin:-'+h+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+a.entry.title.$t+"</span></div>"})}),!1}b(document).ready(function(){c()})})(jQuery)
+/*
+ * YouTube Lite Embed - jQuery plugin to embed light-weight YouTube videos
+ *
+ * Copyright (c) 2012 Amr Tj. Wallas
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project Home:
+ *   https://github.com/TjWallas/YouTube-Lite-Embed
+ * 
+ * Website:
+ *	 http://tjwallas.weebly.com
+ *
+ * Version:  1.0
+ *
+ */
+(function(a){function b(){a("div.lite").each(function(){var b=this;var c=this.id;var d=this.style.width;var e=this.style.height;var f=a(document.createElement("img"));f.attr({"class":"lazy","data-original":"http://img.youtube.com/vi/"+c+"/0.jpg",width:d,height:e});f.css({position:"relative",top:"0",left:"0"});f.lazyload({effect:"fadeIn"});var g=a(document.createElement("a"));g.href="#";g.click(function(){a(this).replaceWith('<embed src="http://www.youtube.com/v/'+c+'?version=3&autoplay=1" type="application/x-shockwave-flash" width="'+d+'" height="'+e+'" allowscriptaccess="always"></embed>');return false});a(b).append(g.append(f,'<img class="lite" src="http://lh4.googleusercontent.com/-QCeB6REIFlE/TuGUlY3N46I/AAAAAAAAAaI/9-urEUtpKcI/s800/youtube-play-button.png" style="position: absolute; top: 50%; left: 45%;" />'));a.getJSON("http://gdata.youtube.com/feeds/api/videos/"+c+"?v=2&fields=id,title&alt=json",function(a){this.innerHTML+='<div style="position:relative;margin:-'+e+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+a.entry.title.$t+"</span></div>"})});return false}a(document).ready(function(){b()})})(jQuery)
