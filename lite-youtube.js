e064f117213c80a0c2637fe3be1aa91d9c6d1807
Amr Tj. Wallas <eminem_virus2010@hotmail.com>
2012-02-13 12:32:17 +0200

    Few performance optimizations and tweaks

--- lite-youtube.js
+++ lite-youtube.js
@@ -15,15 +15,16 @@
  * Version:  1.0
  *
  */
+(function($){ 
 
-$(document).ready(function() {
-  $('div.lite').each(function() {	
-	var myDiv = $(this);
-	var vid = $(this).attr('id');
-	var w = $(this).css('width');
-	var h = $(this).css('height');
+function YTLiteEmbed() {
+$('div.lite').each(function() {	
+	myDiv = this;
+	vid = this.id;
+	w = this.style.width;
+	h = this.style.height;
 
-	var img = $(document.createElement('img'));
+	img = $(document.createElement('img'));
 	img.attr({'class': 'lazy',
 			'data-original': 'http://img.youtube.com/vi/'+vid+'/0.jpg',
 			'width': w,
@@ -34,16 +35,23 @@ $(document).ready(function() {
     	effect : "fadeIn"
 	});
 
-	var a = $(document.createElement('a'));
-	a.attr('href','javascript:void(0)');
+	a = $(document.createElement('a'));
+	a.href='#';
 	a.click(function(){
-		$(this).replaceWith("<embed src=\"http://www.youtube.com/v/"+vid+"?version=3&autoplay=1\" type=\"application/x-shockwave-flash\" width=\""+w+"\" height=\""+h+"\" allowscriptaccess=\"always\"></embed>");
+		$(this).replaceWith("<embed src=\"http://www.youtube.com/v/"+vid+"?version=3&autoplay=1\" type=\"application/x-shockwave-flash\" width=\""+w+"\" height=\""+h+"\" allowscriptaccess=\"always\"></embed>");return false;
 	});
 
-	$(this).append(a.append(img,'<img class="lite" src="http://lh4.googleusercontent.com/-QCeB6REIFlE/TuGUlY3N46I/AAAAAAAAAaI/9-urEUtpKcI/s800/youtube-play-button.png" style="position: absolute; top: 50%; left: 45%;" />'));	
+	$(myDiv).append(a.append(img,'<img class="lite" src="http://lh4.googleusercontent.com/-QCeB6REIFlE/TuGUlY3N46I/AAAAAAAAAaI/9-urEUtpKcI/s800/youtube-play-button.png" style="position: absolute; top: 50%; left: 45%;" />'));	
 
 	$.getJSON('http://gdata.youtube.com/feeds/api/videos/'+vid+'?v=2&fields=id,title&alt=json', function(data) {		
-		myDiv.append('<div style="position:relative;margin:-'+h+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+data.entry.title.$t+'</span></div>');		
+		this.innerHTML+='<div style="position:relative;margin:-'+h+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+data.entry.title.$t+'</span></div>';		
 		});
 	});
+	return false;
+}
+
+$(document).ready(function() {
+	YTLiteEmbed();
 });
+})(jQuery);
+
