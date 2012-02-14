86219c6ebbd101e6c8d4f5f9acebf4d1bcbbe886
Amr Tj. Wallas <eminem_virus2010@hotmail.com>
2012-02-14 11:59:43 +0200

    A few super cool performance tweaks.

--- lite-youtube.js
+++ lite-youtube.js
@@ -18,11 +18,12 @@
 (function($){ 
 
 function YTLiteEmbed() {
-$('div.lite').each(function() {	
-	var myDiv = this;
-	var vid = this.id;
-	var w = this.style.width;
-	var h = this.style.height;
+var liteDivs = $('.lite');
+for (var i = 0; i < liteDivs.length; i++) {
+	var myDiv = liteDivs[i];
+	var vid = myDiv.id;
+	var w = myDiv.style.width;
+	var h = myDiv.style.height;
 
 	var img = $(document.createElement('img'));
 	img.attr({'class': 'lazy',
@@ -31,26 +32,32 @@ $('div.lite').each(function() {
 			'height':h,			
 	});
 	img.css({'position': 'relative', 'top': '0', 'left': '0' });
-	img.lazyload({ 
-    	effect : "fadeIn"
-	});
+	img.lazyload();
 
 	var a = $(document.createElement('a'));
-	a.href='#';
-	a.click(function(){
-		$(this).replaceWith("<embed src=\"http://www.youtube.com/v/"+vid+"?version=3&autoplay=1\" type=\"application/x-shockwave-flash\" width=\""+w+"\" height=\""+h+"\" allowscriptaccess=\"always\"></embed>");return false;
-	});
+	a.href='#';	
 
 	$(myDiv).append(a.append(img,'<img class="lite" src="http://lh4.googleusercontent.com/-QCeB6REIFlE/TuGUlY3N46I/AAAAAAAAAaI/9-urEUtpKcI/s800/youtube-play-button.png" style="position: absolute; top: 50%; left: 45%;" />'));	
 
-	$.getJSON('http://gdata.youtube.com/feeds/api/videos/'+vid+'?v=2&fields=id,title&alt=json', function(data) {		
-		$(myDiv).append('<div style="position:relative;margin:-'+h+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+data.entry.title.$t+'</span></div>');		
-		});
-	});
+	$.ajax({
+  		url: 'http://gdata.youtube.com/feeds/api/videos/'+vid+'?v=2&fields=id,title&alt=json',
+  		dataType: 'json',  		
+  		success: function(data) {
+		$(document.getElementById(data.entry.id.$t.split(':')[3])).append('<div style="position:relative;margin:-'+h+' 5px;padding:5px;background-color:rgba(0,0,0,0.3);-moz-border-radius:7px;-webkit-border-radius:7px;border-radius:7px"><span style="font-weight:bold;font-size:16px;color:#ffffff;font-family:sans-serif;text-align:left;">'+data.entry.title.$t+'</span></div>');		
+		}
+	});	
+	}
 	return false;
 }
 
-$(document).ready(function() {
+var doc = $(document);
+
+doc.on('click', '.lite > a', function(){
+		var div = this.parentNode;		
+		$(div).replaceWith("<embed src=\"http://www.youtube.com/v/"+div.id+"?version=3&autoplay=1\" type=\"application/x-shockwave-flash\" width=\""+div.style.width+"\" height=\""+div.style.height+"\" allowscriptaccess=\"always\"></embed>");return false;
+	});
+
+doc.ready(function() {	
 	YTLiteEmbed();
 });
 })(jQuery);
